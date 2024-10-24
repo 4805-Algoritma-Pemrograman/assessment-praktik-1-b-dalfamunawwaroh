74d3678d46ba20ee641aabc8d17bcb58d2c2e807
Dalfa Munawwarotul Mahmudah <dalfamunawwaroh@gmail.com>
2024-10-24 09:54:51 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,7 +1,30 @@
-
+import java.util.Scanner;
 
 public class Nomor1{
     public static void main(String[] args){
+        
+        double penjualanSalesman = 0;
+        double komisi;
+        double komisiYangDidapatkan;
+        double totalPenjualanSalesman;
+
+
+        Scanner input = new Scanner(System.in);
+            System.out.println("Masukkan penjualan salesman :");
+            penjualanSalesman = input.nextInt();
+
+            if (penjualanSalesman == 500000){
+            } komisi = penjualanSalesman * 0.1;
+                if (penjualanSalesman > 500000){
+                } komisi = penjualanSalesman * 0.5;
+            
+
+            totalPenjualanSalesman = 8 * penjualanSalesman;
+            komisiYangDidapatkan = 0.16 * komisi;
+            
+
+            System.out.println("Total Penjualan Salesman :" + totalPenjualanSalesman);
+            System.out.println("Komisi Yang Didapatkan :" + komisiYangDidapatkan);
       
     }
 }
